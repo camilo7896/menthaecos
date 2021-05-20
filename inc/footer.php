bb5c897c1b55f281dbfe392b3142318f3de4569c
camilo7896 <cg082490@gmail.com>
2021-05-20 17:03:13 -0500

    modificado para mentha ecos

---
+++ inc/footer.php
@@ -0,0 +1,45 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos</h4>
+                <p> 
+                    Manejamos excelente calidad a buenos precios</br>
+                    Contribuimos con el medio ambiente</br>
+                    Generamos empleo</br>
+                    Ayudamos en la economia familiar</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Direccion</h4>
+                <p style="color: #FFF">Duitama Boyacá</p>
+                <p style="color: #FFF">Cll. 14 # 16-65 - piso 5</p>
+                <p style="font-size:20px" aria-hidden="true">  </p> 
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true">+57 310 5057173  /  +57 313 2361040</p> <a href="https://wa.me/message/VO4ACBFA7ADGK1" target="_blank" style="color: #5bc0de">envianos_un_mensaje</a></br>
+                E-mail: <a href="#" target="_blank" style="color: #5bc0de"> &nbsp; menthaecos27@gmail.com</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br> <img src="../assets/img/logo.png" alt="">
+    <h5 class="text-center tittles-pages-logo text-footer">MENTHA ECOS</h5>
+</footer>
---
+++ library/configServer.php
@@ -0,0 +1,5 @@
+<?php
+define("USER", "root");
+define("SERVER", "localhost");
+define("BD", "mentha_ecos");
+define("PASS", "");
